74ea31d64f7c421adaf9511678b13968221cc0f2
akgamerz790 <rockdgyt352@gmail.com>
2026-01-11 11:28:16 +0530

    Create FpsConfig.java

---
+++ src/main/java/dev/akgamerz_790/fpsviewr/config/FpsConfig.java
@@ -0,0 +1,12 @@
+package dev.akgamerz_790.fpsviewr.config;
+
+public class FpsConfig {
+    // Its position on X Axis
+    public int x = 5;
+    // Its position on Y Axis
+    public int y = 5;
+    // Its scale
+    public float scale = 1.0f;
+    // Const 144
+    public boolean fixed144 = false;
+}
